39abc45da672648c65a37cc0222b3fb1c85f41a7
Olle Mattsson <olle.mattsson@gmail.com>
2024-03-21 00:55:37 +0200

    add function for drawing a line between two planets

---
+++ drawPlanetToPlanetLine(p.js
@@ -0,0 +1,32 @@
+import * as THREE from "three";
+
+export function drawPlanetToPlanetLine(planet1, planet2) {
+    p2pPos[0] = planet1.X
+    p2pPos[1] = planet1.Y
+    p2pPos[2] = planet1.Z
+    p2pPos[3] = planet2.X
+    p2pPos[4] = planet2.Y
+    p2pPos[5] = planet2.Z
+    shipLineGeometry.attributes.position.needsUpdate = true;
+
+
+
+    //console.log(start.distanceTo(end))
+
+}
+
+const shipLineMaterial = new THREE.LineBasicMaterial({
+    color: 0xff00ff, 
+});
+const shipLineGeometry = new THREE.BufferGeometry()
+
+export const p2pPos = new Float32Array(2*3) // we need two points with three coordinates each
+
+shipLineGeometry.setAttribute(
+    'position', 
+    new THREE.BufferAttribute(p2pPos, 3)
+)
+
+export const shipLine = new THREE.Line(shipLineGeometry, shipLineMaterial)
+
+
